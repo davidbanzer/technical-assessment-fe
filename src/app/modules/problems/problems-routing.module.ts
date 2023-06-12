0a28546c0d585b1aa82e0375632be8233116d23f
David <davidbanzer123@gmail.com>
2023-06-11 20:59:37 -0400

    feat: Add the welcome page

--- src/app/modules/problems/problems-routing.module.ts
+++ src/app/modules/problems/problems-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChessProblemComponent } from './pages/chess-problem/chess-problem.component';
 import { StringsProblemComponent } from './pages/strings-problem/strings-problem.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
   {
     path: 'problem-1',
     component: ChessProblemComponent,
--- src/app/modules/problems/problems.module.ts
+++ src/app/modules/problems/problems.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ChessProblemComponent } from './pages/chess-problem/chess-problem.component';
 import { StringsProblemComponent } from './pages/strings-problem/strings-problem.component';
 import { ProblemsRoutingModule } from './problems-routing.module';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 
 
 @NgModule({
   declarations: [
     ChessProblemComponent,
-    StringsProblemComponent
+    StringsProblemComponent,
+    WelcomeComponent
   ],
   imports: [
     CommonModule,
